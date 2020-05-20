4898884c1872c4e8f24c1d3211fdb8d804e257c0
Antony.Glim <antony.glim@gmail.com>
2020-05-20 21:28:33 +0300

    Add file generator

---
+++ src/main/java/glim/antony/generator/Generator.java
@@ -0,0 +1,25 @@
+package glim.antony.generator;
+
+import glim.antony.generator.dictionaries.AuthorDictionary;
+import glim.antony.generator.dictionaries.QuoteDictionary;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.SplittableRandom;
+
+public class Generator {
+
+    private static final int ROWS_COUNT = 10;
+    private static final String FILE_NAME = "QuotesAndAuthors.txt";
+    public static final SplittableRandom RANDOM = new SplittableRandom();
+
+    public static void main(String[] args) throws IOException {
+        FileWriter fileWriter = new FileWriter(FILE_NAME, true);
+        for (int i = 0; i < ROWS_COUNT; i++) {
+            String quote = QuoteDictionary.getQUOTES()[RANDOM.nextInt(0, QuoteDictionary.getQUOTES().length)];
+            String author = AuthorDictionary.getAUTHORS()[RANDOM.nextInt(0, AuthorDictionary.getAUTHORS().length)];
+            fileWriter.write(quote + "\t" + author + "\n");
+            fileWriter.flush();
+        }
+    }
+}
