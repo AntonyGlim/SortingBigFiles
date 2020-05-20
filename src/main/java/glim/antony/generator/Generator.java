eadcc32b268b072bad6db697d61366f1d1f8190f
Antony.Glim <antony.glim@gmail.com>
2020-05-21 00:15:11 +0300

    Change data to English words (reason: encoding)

--- src/main/java/glim/antony/generator/Generator.java
+++ src/main/java/glim/antony/generator/Generator.java
@@ -11,15 +11,16 @@ public class Generator {
 
     private static final int ROWS_COUNT = 10;
     private static final String FILE_NAME = "QuotesAndAuthors.txt";
-    public static final SplittableRandom RANDOM = new SplittableRandom();
+    private static final SplittableRandom RANDOM = new SplittableRandom();
 
     public static void main(String[] args) throws IOException {
-        FileWriter fileWriter = new FileWriter(FILE_NAME, true);
-        for (int i = 0; i < ROWS_COUNT; i++) {
-            String quote = QuoteDictionary.getQUOTES()[RANDOM.nextInt(0, QuoteDictionary.getQUOTES().length)];
-            String author = AuthorDictionary.getAUTHORS()[RANDOM.nextInt(0, AuthorDictionary.getAUTHORS().length)];
-            fileWriter.write(quote + "\t" + author + "\n");
-            fileWriter.flush();
+        try(FileWriter fileWriter = new FileWriter(FILE_NAME, true);) {
+            for (int i = 0; i < ROWS_COUNT; i++) {
+                String quote = QuoteDictionary.getQUOTES()[RANDOM.nextInt(0, QuoteDictionary.getQUOTES().length)];
+                String author = AuthorDictionary.getAUTHORS()[RANDOM.nextInt(0, AuthorDictionary.getAUTHORS().length)];
+                fileWriter.write(quote + "\t" + author + "\n");
+                fileWriter.flush();
+            }
         }
     }
 }
